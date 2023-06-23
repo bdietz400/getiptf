28e39ab12ab350cdb4885df4ac2d9013cc5a4a38
bryan_d <bdietz400@gmail.com>
2023-06-23 12:22:46 -0500

    Create Get_i_PTF.sh

---
+++ Get_i_PTF.sh
@@ -0,0 +1,34 @@
+#!/QOpenSys/pkgs/bin/bash
+
+#  Bryan Dietz   06/23/2023
+#  *** work in progress***
+#  for now install in directory /ptfs
+# make sure rpm pile is install plus "lftp"
+
+
+
+echo "sFTP User Name: "
+read s_user
+echo "Remote Server: "
+read rmt_srv
+echo "Remote Directory: "
+read rmt_dir
+echo "Local Directory: "
+read lcl_dir
+echo "User Password: "
+read u_pwd
+
+echo "-----------------"
+echo "sFTP User Name  : ${s_user}"
+echo "Remote Server   : ${rmt_srv}"
+echo "Remote Directory: ${rmt_dir}"
+echo "Local Directory : ${lcl_dir}"
+echo "User Password   : ${u_pwd}"
+echo "-----------------"
+
+/QOpenSys/pkgs/bin/lftp -c "set sftp:auto-confirm on; set xfer:clobber on; \
+set xfer:log-file /ptfs/my_lftp.log; \
+open -u $s_user,$u_pwd \
+sftp://$rmt_srv;lcd $lcl_dir;\
+cd $rmt_dir;ls;mirror --verbose --use-pget-n=2 -c --verbose . $lcl_dir"  
+ls -la $lcl_dir
